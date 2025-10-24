960bd982a4041bbbdc036de39eeafe123931e6e4
Will Schuhmann <wps2401@gmail.com>
2025-10-23 21:06:25 -0500

    Setup with VS Code

---
+++ ch1_arrays_and_strings/1.1/1.1.cpp
@@ -0,0 +1,8 @@
+// /c:/git-workspace/cracking-the-coding-interview/ch1_arrays_and_strings/1.1.cpp
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    std::cout << "Hello, C++ world!\n";
+    // TODO: implement solution for Cracking the Coding Interview - 1.1
+    return 0;
+}

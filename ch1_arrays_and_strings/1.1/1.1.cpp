b4196d85e91a3c268d48b1b2529513166f0368c0
Will Schuhmann <wps2401@gmail.com>
2025-10-25 11:34:46 -0500

    Solution

--- ch1_arrays_and_strings/1.1/1.1.cpp
+++ ch1_arrays_and_strings/1.1/1.1.cpp
@@ -1,9 +1,32 @@
 //1.1 - Is Unique
 
 #include <iostream>
+#include <unordered_map>
+#include <string>
+
+bool hasDuplicateChars(const std::string& inputString){
+    std::unordered_map<char, bool> myHashTable;
+
+    for (char i : inputString) {    //For each char i in input string
+        if (myHashTable[i]) {
+            return true; // duplicated chars
+        }
+        myHashTable[i] = true;
+    }
+
+    return false;   //No duplicated chars
+}
 
 int main() {
-    std::cout << "Hello, C++ world!\n";
-    // TODO: implement solution for Cracking the Coding Interview - 1.1
+
+    std::string input;
+    std::cout << "Enter a string: ";
+    std::cin >> input;
+
+    if (hasDuplicateChars(input))
+        std::cout << "Not Unique\n";
+    else
+        std::cout << "Unique\n";
+
     return 0;
 }

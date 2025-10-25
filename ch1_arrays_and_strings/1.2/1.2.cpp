48d532ee9354c5680157eb29c6c68179f777f932
Will Schuhmann <wps2401@gmail.com>
2025-10-25 14:41:10 -0500

    Main() of 1.2

--- ch1_arrays_and_strings/1.2/1.2.cpp
+++ ch1_arrays_and_strings/1.2/1.2.cpp
@@ -1,8 +1,26 @@
 //1.2 - Check Permutation
 
 #include <iostream>
+#include <unordered_map>
+#include <string>
+
+bool isPermutation(const std::string& inputString1, const std::string& inputString2){
+    return true;
+}
 
 int main() {
-    std::cout << "Test";
+
+    std::string input1;
+    std::cout << "Enter first string: ";
+    std::cin >> input1;
+
+    std::string input2;
+    std::cout << "Enter second string: ";
+    std::cin >> input2;
+
+    if (isPermutation(input1, input2))
+        std::cout << input2 + " IS a permutation of " + input1 + "\n";
+    else
+        std::cout << input2 + " IS NOT a permutation of " + input1 + "\n";
     return 0;
 }

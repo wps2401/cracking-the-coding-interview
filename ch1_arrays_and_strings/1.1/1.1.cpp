76072aaaaafcbe23991c976db707aed7af74e5d0
Will Schuhmann <wps2401@gmail.com>
2025-10-24 23:35:00 -0500

    Adding to ReadMe and removing unused arugments

--- ch1_arrays_and_strings/1.1/1.1.cpp
+++ ch1_arrays_and_strings/1.1/1.1.cpp
@@ -1,7 +1,8 @@
-// /c:/git-workspace/cracking-the-coding-interview/ch1_arrays_and_strings/1.1.cpp
+//1.1 - Is Unique
+
 #include <iostream>
 
-int main(int argc, char* argv[]) {
+int main() {
     std::cout << "Hello, C++ world!\n";
     // TODO: implement solution for Cracking the Coding Interview - 1.1
     return 0;
